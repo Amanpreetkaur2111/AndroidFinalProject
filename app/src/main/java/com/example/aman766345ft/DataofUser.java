e7c96084928f244a08f45f48a002f88a43c387ec
Amanpreetkaur <aman021011@gmail.com>
2019-12-13 14:48:55 -0500

    Toast Message and ArrayList

--- app/src/main/java/com/example/aman766345ft/DataofUser.java
+++ app/src/main/java/com/example/aman766345ft/DataofUser.java
@@ -1,4 +1,19 @@
 package com.example.aman766345ft;
 
-public class DataofUser {
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class DataofUser  {
+
+   public String StoreName;
+   public String StoreEmail;
+    public String StorePhone;
+
+    public DataofUser(String storeName, String storeEmail, String storePhone) {
+        StoreName = storeName;
+        StoreEmail = storeEmail;
+        StorePhone =  storePhone;
+    }
+
+public static ArrayList<String> DetailsofUser = new ArrayList<>();
 }
--- app/src/main/java/com/example/aman766345ft/Register_User.java
+++ app/src/main/java/com/example/aman766345ft/Register_User.java
@@ -2,13 +2,54 @@ package com.example.aman766345ft;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class Register_User extends AppCompatActivity {
+import java.io.Serializable;
+
+public class Register_User extends AppCompatActivity  {
+
+    EditText n_text,e_text,p_text;
+    Button s_btn;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register__user);
+
+        // Edit Text
+        n_text = findViewById(R.id.name);
+        e_text = findViewById(R.id.email);
+        p_text = findViewById(R.id.phone);
+
+        // Save Button
+
+        s_btn = findViewById(R.id.savebtn);
+
+        s_btn.setOnClickListener(new View.OnClickListener() {
+
+
+
+            @Override
+            public void onClick(View view) {
+                if (!n_text.getText().toString().isEmpty() & e_text.getText().toString().isEmpty() & p_text.getText().toString().isEmpty()){
+
+                    DataofUser data = new DataofUser(n_text.getText().toString(),e_text.getText().toString(),p_text.getText().toString());
+                    Toast.makeText(Register_User.this,"Saved Successfully",Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    Toast.makeText(Register_User.this,"All the fields are Mandatory",Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+
     }
 }
